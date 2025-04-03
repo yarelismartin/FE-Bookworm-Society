e53393403dee1585fb84f4b71a407a0d3d4d9625
Yarelis Martin <yarelis0martin@gmail.com>
2025-04-03 16:45:53 -0500

    made a skeleton layout of the page and grabbed the data needed yo display book details

--- src/app/books/[bookId]/page.js
+++ src/app/books/[bookId]/page.js
@@ -16,5 +16,33 @@ export default function BookDetail() {
     getBook();
     console.warn(book);
   }, []);
-  return <div>book detail</div>;
+  return (
+    <div>
+      <div className="book-image-container">
+        <div className="book-image">
+          <p>{book.imageUrl}</p>
+        </div>
+        <div className="CTA-rate">
+          <p>rate this book CTA links to review form</p>
+        </div>
+      </div>
+      <div className="book-helights">
+        <h2>{book.title}</h2>
+        <h4>{book.author} </h4>
+        <h2>star rating when clicked takes you down to the reviews</h2>
+        <p>{book.description}</p>
+        <p>{book.genre}</p>
+      </div>
+      <div className="book-reviews">
+        <div className="filter-by-rating">
+          <p>1 -5 star filter with the percentage showcases beside the start line</p>
+        </div>
+        <div className="users-reviews">
+          {book.reviews.map((review) => (
+            <p>{review.content}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
