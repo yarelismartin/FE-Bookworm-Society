214612aaaf3229bd90504258b30d2f082658b87d
Yarelis Martin <yarelis0martin@gmail.com>
2025-04-03 23:01:28 -0500

    creating the review form

--- src/app/books/[bookId]/page.js
+++ src/app/books/[bookId]/page.js
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
+import Link from 'next/link';
 import { getSingleBook } from '../../../api/BookData';
 
 export default function BookDetail() {
@@ -15,7 +16,9 @@ export default function BookDetail() {
   useEffect(() => {
     getBook();
     console.warn(book);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <div>
       <div className="book-image-container">
@@ -23,10 +26,12 @@ export default function BookDetail() {
           <p>{book.imageUrl}</p>
         </div>
         <div className="CTA-rate">
-          <p>rate this book CTA links to review form</p>
+          <Link href={`/books/${bookId}/review/create`} passHref>
+            <p>rate this book CTA links to review form</p>
+          </Link>
         </div>
       </div>
-      <div className="book-helights">
+      <div className="book-highlight">
         <h2>{book.title}</h2>
         <h4>{book.author} </h4>
         <h2>star rating when clicked takes you down to the reviews</h2>
@@ -38,7 +43,7 @@ export default function BookDetail() {
           <p>1 -5 star filter with the percentage showcases beside the start line</p>
         </div>
         <div className="users-reviews">
-          {book.reviews.map((review) => (
+          {book.reviews?.map((review) => (
             <p>{review.content}</p>
           ))}
         </div>
